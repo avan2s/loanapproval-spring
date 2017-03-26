e6a23a9502de7b09cbc0150d88b361a42026204d
Andy <andreas.van@smail.inf.h-brs.de>
2017-03-26 22:50:56 +0200

    hu8uh

--- src/main/java/org/camunda/bpm/services/TaskActionFinder.java
+++ src/main/java/org/camunda/bpm/services/TaskActionFinder.java
@@ -3,7 +3,6 @@ package org.camunda.bpm.services;
 import org.camunda.bpm.engine.FormService;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.form.TaskFormData;
-import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.model.TaskAction;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,7 +21,7 @@ public class TaskActionFinder {
     public List<TaskAction> getTaskActions(String taskId){
         FormService formService = processEngine.getFormService();
         TaskFormData taskFormData = formService.getTaskFormData(taskId);
-        
+        return null;
     }
 
     public void setProcessEngine(ProcessEngine processEngine) {
