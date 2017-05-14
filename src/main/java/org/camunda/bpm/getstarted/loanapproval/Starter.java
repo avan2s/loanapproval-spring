bd3b4c085ad92f51c7457e949ca45b3add15cc67
Andy <andreas.van@smail.inf.h-brs.de>
2017-05-14 19:07:13 +0200

    initial

--- src/main/java/org/camunda/bpm/getstarted/loanapproval/Starter.java
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/Starter.java
@@ -1,8 +1,12 @@
 package org.camunda.bpm.getstarted.loanapproval;
 
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * Created by Andy on 25.03.2017.
@@ -13,7 +17,9 @@ public class Starter implements InitializingBean {
     private RuntimeService runtimeService;
 
     public void afterPropertiesSet() throws Exception {
-        //runtimeService.startProcessInstanceByKey("loanApproval","businessKey-123");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("loanApproval", "businessKey-123");
+        List<Execution> executionList = runtimeService.createExecutionQuery().messageEventSubscriptionName("m").list();
+        System.out.println(executionList);
     }
 
     public void setRuntimeService(RuntimeService runtimeService) {
